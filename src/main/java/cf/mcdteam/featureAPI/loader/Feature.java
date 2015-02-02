fb56e005916e13c9cc4fea4ea2d5a600942d6df1
bcwadsworth <bcwadsworth@gmail.com>
2015-02-01 21:58:16 -0700

    Feature Annotation Work

--- src/main/java/cf/mcdteam/featureAPI/loader/Feature.java
+++ src/main/java/cf/mcdteam/featureAPI/loader/Feature.java
@@ -15,99 +15,35 @@ public @interface Feature
 	/**
 	 * The internal name of the feature
 	 */
-    public String name() default "UNNAMED";
-    
-    /**
-     * The version of the feature
-     */
-    public String version() default "UNVERSIONED";
-    
-    /**
-     * Whether or not the feature is a base feature
-     * if enabled, cannot be turned off
-     */
-    public boolean isBase() default false;
-    
-    /**
-     * If required and has this as true, @FeatureData(Data.Alternate) will be true if turned off
-     * All code will run. The feature is responsible to do processing based on this.
-     */
-    public boolean hasDisabledCompatility() default false;
-    
+    public String value() default "UNNAMED";
+
     /**
      * The interface responsible for telling the code how to use methods within the feature class
      * NOTE: EVERY METHOD WITHIN THE FEATURE THAT WILL BE USED BY BEGINNING CODE
      * MUST HAVE ONE OF THESE ANNOTATIONS
-     * NONSETUP will not run
+     * Anything that does not have a valid string identifier will not run
      */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.METHOD)
     public @interface FeatureElement 
-    {
+    {  	
     	/**
-    	 * The Types of events
-    	 * FORGE EVENT TYPES SHOULD ONLY BE USED IF WHAT
-    	 * YOU WANT TO DO IS NOT INCLUDED ON THE LIST
-    	 * NONSETUP is not called naturally
+    	 * The type of the method, designated by the internal string that corresponds with a feature
+    	 * loader method
     	 */
-    	public enum Element
-    	{
-    		CONFIGURATION,
-    		OBJECT,
-    		EVENTBUS_ORE,
-    		EVENTBUS_TERRAIN,
-    		EVENTBUS_EVENT,
-    		ENTITY,
-    		MOD_COMPATIBILITY,
-    		PREINITIALIZATION,
-    		INTITIALIZATION,
-    		POSTINITIALIZATION,
-    		SERVERABOUTTOSTART,
-    		SERVERSTARTING,
-    		SERVERSTARTED,
-    		SERVERSTOPPING,
-    		SERVERSTOPPED,
-    		NONSETUP,
-            CLIENT
-    	}
-    	
-    	/**
-    	 * The type of the method
-    	 */
-    	public Element value() default Element.NONSETUP; 
+    	public String value() default "None"; 
     }
     
     /**
-     * This is used for marking variables for the populator to populate
-     * during setup and beyond if needed. If your stuff is not the correct 
-     * type or not public, crashes will result
+     * This annotation marks a boolean which determines whether the feature is active. Each of the variables
+     * inside the annotation mark its behavior
      */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.FIELD)
-    public @interface FeatureData
+    public @interface FeatureActivator
     {
-    	/**
-    	 * The Types of data given by the populator
-    	 * NONDATA will not be populated
-    	 */
-    	public enum Data
-    	{
-    		LOGGER,
-    		MODINSTANCE,
-    		PREFEATURELIST,
-    		FULLFEATURELIST,
-    		ALTFEATURELIST,
-    		COMPLETEFEATURELIST,
-    		ALTERNATE,
-    		FEATUREMAP,
-    		FEATUREOBJECTMAP,
-    		NONDATA
-    	}
-    	
-    	/**
-    	 * The type of the method
-    	 */
-    	public Data value() default Data.NONDATA;
+    	public boolean base() default false;
+    	public String[] dependencies() default new String[]();
     }
 }
 
